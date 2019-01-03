0ebeb7fda724f2697b927b573d2df486b2905ae0
Roberto Schioppa <roberto.schioppa@propylon.com>
2019-01-03 16:22:10 +0000

    Automatically finds out the file extension from the name

--- src/image-resizer.js
+++ src/image-resizer.js
@@ -4,11 +4,17 @@ const { exec } = require('child_process');
 const cli = require('./cli');
 const breakpoints = require('./breakpoints');
 
-const imageFile = path.join(__dirname, cli.getFlag('imageFile'));
+const imageFile = cli.getFlag('imageFile');
+const extension = imageFile.substring(
+  imageFile.lastIndexOf('.') + 1,
+  imageFile.length
+);
+const filePath = path.join(__dirname, imageFile);
 
+const extensionRegExp = new RegExp(`.${extension}$`, 'i');
 breakpoints.forEach((res) => {
-  const dest = imageFile.replace(/.jpg$/, `-${res}w.jpg`);
+  const destFile = filePath.replace(extensionRegExp, `-${res}w.${extension}`);
 
-  console.log(`generating ${dest}`);
-  exec(`convert -resize ${res}x${res} ${imageFile} ${dest}`);
+  console.log(`generating ${destFile}`);
+  exec(`convert -resize ${res}x${res} ${filePath} ${destFile}`);
 });
